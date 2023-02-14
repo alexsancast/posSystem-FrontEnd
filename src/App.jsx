f27f6f3390f368056419a48bd15065542951b608
Alexander <alexsancast@outlook.com>
2023-02-14 13:52:23 -0400

    Probando el sasss

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,10 @@
 import { useState } from "react";
-
+import Login from "./components/Login";
 
 function App() {
   const [count, setCount] = useState(0);
 
-  return <div> Este es el login </div>;
+  return <Login />;
 }
 
 export default App;
---
+++ src/components/Login.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Login = () => {
+  return <div> Hola Este es el login </div>;
+};
+
+export default Login;

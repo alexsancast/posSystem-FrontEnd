317d5f26839e72c3fbd4d73458a96b47acd3f5a4
Alexander <alexsancast@outlook.com>
2023-02-21 14:56:11 -0400

    Agregando los iconos al tablero

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -2,13 +2,22 @@ import React from "react";
 import "../sass/main.scss";
 import { GiHamburgerMenu } from "react-icons/gi";
 import { FaUserCircle } from "react-icons/fa";
+import { TbTableExport } from "react-icons/tb";
+import { SlBasketLoaded } from "react-icons/sl";
+
+SlBasketLoaded
+
 
 const Main = () => {
   return <div className="container__main">
+
+          
          <div className="container__bar-horizontal"><GiHamburgerMenu size="2.5em"/>
          
          
          </div>
+
+      
 
 
          <div className="container__bar-vertical">
@@ -16,8 +25,18 @@ const Main = () => {
           <div className="container__username-profile">
              
              <FaUserCircle size="2em"/>
-             <p className="username__name">Hola Alexander</p>
+             <p className="main__label-username">Hola Alexander</p>
 
+          </div>
+
+          <div className= "container__main-menu container__menu">
+            <TbTableExport size="1.7em"/>
+            <p className="main__label-menu">Tablero principal</p>
+          </div>
+
+          <div className= "container__caja-menu container__menu">
+             <SlBasketLoaded size="1.7em"/>
+            <p className="main__label-menu">Caja</p>
           </div>
 
 

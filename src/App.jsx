c38b0b0f8ce3bad9c91568cea9f3a949f31e0938
Alexander <alexsancast@outlook.com>
2023-02-21 10:14:49 -0400

    Arreglando las fuentes

--- src/App.jsx
+++ src/App.jsx
@@ -2,9 +2,17 @@ import { Login } from "./components/Login";
 import { useFormik } from "formik";
 import * as Yup from "yup";
 
+
 function App() {
   
-  return <Login/>;
+  return (
+      
+      
+
+    <Login/> 
+ 
+    
+  ) ;
 }
 
 export default App;

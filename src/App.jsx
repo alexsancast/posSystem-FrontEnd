ae355e5f2d24f7352d9776fd4bc99ea3f3e833e6
Alexander <alexsancast@outlook.com>
2023-02-22 15:11:32 -0400

    Agregando los comentarios en el css y eliminando el icono de la pestana

--- src/App.jsx
+++ src/App.jsx
@@ -3,9 +3,13 @@ import { useFormik } from "formik";
 import * as Yup from "yup";
 import Main from "./components/Main";
 
+
 function App() {
   return (
-    <Main />
+    
+    
+    <Main  />
+
     // <Login/>
   );
 }
--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -8,28 +8,22 @@ import { FaBoxes , FaUserEdit,FaUserCircle , FaTools} from "react-icons/fa";
 import { HiDocumentReport } from "react-icons/hi";
 import { IoMdExit } from "react-icons/io";
 
-
-
-
-
-
-
-
-
-
-
-
-
 const Main = () => {
   return <div className="container__main">
+
+
 
           
          <div className="container__bar-horizontal">
             <GiHamburgerMenu className="main__icon   " size="2.5em"/>
             <h1 className="title__homepage">Bienvenidos a Dragon Lite</h1>
          
-         </div>
+         </div> 
 
+        
+
+           
+  
       
           
          <div className="container__bar-vertical">
@@ -46,42 +40,42 @@ const Main = () => {
             <p className="main__label-menu">Tablero principal</p>
           </div>
 
-          <div className= "container__caja-menu container__menu">
+          <div className= "container__menu-caja container__menu">
              <FaMoneyBill className="main__icon" size="1.7em"/>
             <p className="main__label-menu">Caja</p>
           </div>
 
-          <div className= "container__products-menu container__menu">
+          <div className= "container__menu-products container__menu">
              <RiInboxFill className="main__icon" size="1.7em"/>
             <p className="main__label-menu">Productos</p>
           </div>
 
-          <div className= "container__products-menu container__menu">
+          <div className= "container__menu-shopping container__menu">
              <FaBoxes className="main__icon" size="1.7em"/>
             <p className="main__label-menu">Compras</p>
           </div>
 
-          <div className= "container__products-menu container__menu">
+          <div className= "container__menu-products container__menu">
              <FaUserEdit className="main__icon" size="1.7em"/>
             <p className="main__label-menu">Contactos</p>
           </div>
 
-          <div className= "container__products-menu container__menu">
+          <div className= "container__menu-reports container__menu">
              <HiDocumentReport className="main__icon" size="1.7em"/>
             <p className="main__label-menu">Reportes</p>
           </div>
 
-          <div className= "container__products-menu container__menu">
+          <div className= "container__menu-users container__menu">
              <FaUserCog className="main__icon" size="1.7em"/>
             <p className="main__label-menu">Usuarios & Roles</p>
           </div>
 
-          <div className= "container__products-menu container__menu">
+          <div className= "container__menu-tools container__menu">
              <FaTools className="main__icon" size="1.7em"/>
             <p className="main__label-menu">Configuraciones</p>
           </div>
 
-          <div className= "container__products-menu container__menu-exit">
+          <div className= "container__menu-exit container__menu-exit">
              <IoMdExit className="main__icon" size="1.7em"/>
             <p className="main__label-menu">Salir</p>
           </div>

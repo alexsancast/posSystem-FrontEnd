6b1c459c7b17bc18a136bf7ec4b5217965c83a4b
Alexander <alexsancast@outlook.com>
2023-02-28 09:01:31 -0400

    Agregando los botones

--- src/App.jsx
+++ src/App.jsx
@@ -2,14 +2,14 @@ import { Login } from "./components/Login";
 import { useFormik } from "formik";
 import * as Yup from "yup";
 import {Navbarhorizontal , Navbarvertical} from "./components/Navbars";
+import { ButtonS , Button , ButtonM } from "./components/Buttons";
 
 
 function App() {
   return (
     
      <>
-        <Navbarhorizontal/>
-          <Navbarvertical/>
+        <Button color = {false} label = "Cancelar"/>
      </>
   
       
